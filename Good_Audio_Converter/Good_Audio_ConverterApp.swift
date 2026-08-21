382bb990f01362695291e9ee639d6e15714e4ff8
Elliott Miles <elliottswims5@gmail.com>
2026-08-20 18:39:14 -0600

    Initial Commit

---
+++ Good_Audio_Converter/Good_Audio_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Good_Audio_ConverterApp.swift
+//  Good_Audio_Converter
+//
+//  Created by Agent Sandbox on 8/20/26.
+//
+
+import SwiftUI
+
+@main
+struct Good_Audio_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Good_Audio_ConverterTests/Good_Audio_ConverterTests.swift
@@ -0,0 +1,19 @@
+//
+//  Good_Audio_ConverterTests.swift
+//  Good_Audio_ConverterTests
+//
+//  Created by Agent Sandbox on 8/20/26.
+//
+
+import Testing
+@testable import Good_Audio_Converter
+
+struct Good_Audio_ConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
